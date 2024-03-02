71f28433a7efbd46c5b8c5bd86e6b6cb1baa3a71
rohan <rohansureshgupta@gmail.com>
2024-03-02 17:08:45 +0530

    added 2nd march

---
+++ SquaresofaSortedArray.cpp
@@ -0,0 +1,26 @@
+class Solution
+{
+public:
+    vector<int> sortedSquares(vector<int> &nums)
+    {
+        for (int i = 0; i < nums.size(); i++)
+            nums[i] = nums[i] * nums[i];
+        int l = 0;
+        int r = nums.size() - 1;
+        vector<int> res(nums.size());
+        for (int i = nums.size() - 1; i >= 0; i--)
+        {
+            if (nums[l] > nums[r])
+            {
+                res[i] = nums[l];
+                l++;
+            }
+            else
+            {
+                res[i] = nums[r];
+                r--;
+            }
+        }
+        return res;
+    }
+};
