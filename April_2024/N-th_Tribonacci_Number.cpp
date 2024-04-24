a619275edaae313c87f06d074386b77f3ae0c504
rohan <rohansureshgupta@gmail.com>
2024-04-24 16:55:23 +0530

    Added 24th April

---
+++ April_2024/N-th_Tribonacci_Number.cpp
@@ -0,0 +1,23 @@
+class Solution
+{
+public:
+    int f(int n, vector<int> &dp)
+    {
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+        if (n == 2)
+            return 1;
+        if (n < 0)
+            return 0;
+        if (dp[n] != -1)
+            return dp[n];
+        return dp[n] = f(n - 3, dp) + f(n - 2, dp) + f(n - 1, dp);
+    }
+    int tribonacci(int n)
+    {
+        vector<int> dp(n + 1, -1);
+        return f(n, dp);
+    }
+};
