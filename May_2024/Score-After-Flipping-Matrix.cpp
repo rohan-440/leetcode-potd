a14a7365992ee9bdcf92bafc734c47b9096d6619
rohan <rohansureshgupta@gmail.com>
2024-05-13 12:00:37 +0530

    Added 13th May 2024

---
+++ May_2024/Score-After-Flipping-Matrix.cpp
@@ -0,0 +1,43 @@
+class Solution
+{
+public:
+    void fliprow(vector<vector<int>> &grid, int m, int row)
+    {
+        for (int i = 0; i < m; i++)
+            grid[row][i] ^= 1;
+    }
+    void flipcol(vector<vector<int>> &grid, int n, int col)
+    {
+        for (int i = 0; i < n; i++)
+            grid[i][col] ^= 1;
+    }
+
+    int matrixScore(vector<vector<int>> &grid)
+    {
+        int n = grid.size(), m = grid[0].size();
+
+        for (int i = 0; i < n; i++)
+            if (!grid[i][0])
+                fliprow(grid, m, i);
+
+        for (int j = 0; j < m; j++)
+        {
+            int zero = 0;
+            for (int i = 0; i < n; i++)
+                if (grid[i][j] == 0)
+                    zero++;
+            if (zero > (n - zero))
+                flipcol(grid, n, j);
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n; i++)
+        {
+            int num = 0;
+            for (int j = 0; j < m; j++)
+                num += grid[i][j] * pow(2, m - 1 - j);
+            ans += num;
+        }
+        return ans;
+    }
+};
