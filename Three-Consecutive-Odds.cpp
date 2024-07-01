97b8ce5f32c00e3c05d84c25e134c6dad7d54787
rohan <rohansureshgupta@gmail.com>
2024-07-01 06:02:56 +0530

    1st July 2024

---
+++ Three-Consecutive-Odds.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+public:
+    bool threeConsecutiveOdds(vector<int> &arr)
+    {
+        for (int i = 2; i < arr.size(); i++)
+        {
+            if (arr[i] % 2 != 0)
+            {
+                if (arr[i - 1] % 2 != 0 && arr[i - 2] % 2 != 0)
+                    return 1;
+            }
+        }
+        return 0;
+    }
+};
