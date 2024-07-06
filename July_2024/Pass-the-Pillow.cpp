813304f90bcfc007274629aaca7774e43814c37c
rohan <rohansureshgupta@gmail.com>
2024-07-06 10:05:44 +0530

    6th July 2024

---
+++ July_2024/Pass-the-Pillow.cpp
@@ -0,0 +1,12 @@
+class Solution
+{
+public:
+    int passThePillow(int n, int time)
+    {
+        int div = time / (n - 1);
+        int rem = time % (n - 1);
+        if (div % 2 == 1)
+            return n - rem;
+        return rem + 1;
+    }
+};
